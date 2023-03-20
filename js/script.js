b9f8e533daa77b8434679cce1839fdb1e5f02bdb
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2023-03-20 23:03:43 +0200

    з

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 
 
 window.onload = () => {
+
+    let c = function (a) {
+        console.log(a)
+    }
+
+    c(Object.keys(document.querySelectorAll('.mainPage li')).length);
+    c(Object.keys(document.querySelectorAll('.istorichniFotoPage li')).length);
+
+    //------------------------------------------------------------------------------------------------------------------------------
+
+
     $('.in').hide();
+
+    //------------------------------------------------------------------------------------------------------------------------------
+
 
     // Відкривання меню
     $('.header_burger').click(function (event) {
         $('.header_burger,.header_menu').toggleClass('active');
-        // $('body').toggleClass('lock');
-        console.log(1);
     });
 
-    console.log(0);
+
+    //------------------------------------------------------------------------------------------------------------------------------
+
 
     // Закривання меню
     $('.list_menuLi').click(function (event) {
         $('.header_burger,.header_menu').removeClass('active');
-        // $('body').removeClass('lock');
     });
 
-
-
+    //------------------------------------------------------------------------------------------------------------------------------
 
 
     const scrollBtn = document.querySelector('.isShowBtn')
@@ -35,38 +47,44 @@ window.onload = () => {
         window.scrollTo(0, 0)
     }
 
+    //------------------------------------------------------------------------------------------------------------------------------
+
     let input = document.querySelector('#input');
-
-
-
     input.oninput = function () {
         let valu = this.value.trim();
         let value = valu.toUpperCase();
-
         let list = document.querySelectorAll('.mainPage li');
-        // console.log(Object.keys(list).length); // - всего обектов
+        let listIF = document.querySelectorAll('.istorichniFotoPage li');
 
 
         if (value != '') {
-            list.forEach(elem => {
-                // console.log(elem.innerText.length);
-                let a = elem.innerText.search(value);
-                // console.log(a);
-                // console.log(a);
 
-
+            listIF.forEach(elem => {
 
                 if (elem.innerText.search(value) == -1) {
                     elem.classList.add('hide')
+                }
+                $('h1').click(function (event) {
+                    $('#input').val('');
+                    elem.classList.remove('hide');
+                    $('.in').hide();
+                });
+            });
 
 
 
-                    // console.log($('.hide').length);
 
 
-                    // console.log(elem.classList); //typeof()
 
 
+
+
+
+
+            list.forEach(elem => {
+
+                if (elem.innerText.search(value) == -1) {
+                    elem.classList.add('hide')
                 }
                 $('h1').click(function (event) {
                     $('#input').val('');
@@ -78,9 +96,12 @@ window.onload = () => {
             list.forEach(elem => {
                 elem.classList.remove('hide');
             });
+
+            listIF.forEach(elem => {
+                elem.classList.remove('hide');
+            });
         }
         if ($('.hide').length == Object.keys(list).length) {
-            // document.write('Пусто')
             $('.in').show();
 
         } else {
@@ -89,11 +110,15 @@ window.onload = () => {
         }
     }
 
-
+    //------------------------------------------------------------------------------------------------------------------------------
 
     setTimeout(() => {
         // document.footer.classList.add('loaded');
         $('footer').toggleClass('loaded');
     }, 1);
+
+
+
+
 
 };
