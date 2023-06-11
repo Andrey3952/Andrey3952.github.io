d826e58edefd2d5d4bda659c82ba9b36a571dc3a
Andrey3952 <60111540+Andrey3952@users.noreply.github.com>
2023-06-11 10:55:00 +0300

    а

--- js/script.js
+++ js/script.js
@@ -49,66 +49,98 @@ window.onload = () => {
 
     //------------------------------------------------------------------------------------------------------------------------------
 
-    let input = document.querySelector('#input');
-    input.oninput = function () {
-        let valu = this.value.trim();
-        let value = valu.toUpperCase();
-        let list = document.querySelectorAll('.mainPage li');
-        let listIF = document.querySelectorAll('.istorichniFotoPage li');
+    function search() {
+        let input = document.getElementById("input");
+        let filter = input.value.toUpperCase();
+        let ul = document.getElementById("list");
+        let li = ul.getElementsByTagName("li");
+        // let listIF = document.querySelectorAll('.istorichniFotoPage li');
+        // console.log(ul);
 
 
-        if (value != '') {
+        // Перебирайте все элементы списка и скрывайте те, которые не соответствуют поисковому запросу
+        for (let i = 0; i < li.length; i++) {
+            let a = li[i].getElementsByTagName("p")[0];
+            if (li[i].getElementsByTagName("p")[0] != null) {
 
-            listIF.forEach(elem => {
 
-                if (elem.innerText.search(value) == -1) {
-                    elem.classList.add('hide')
+                if (a.innerHTML.toUpperCase().indexOf(filter) == -1) {
+                    li[i].classList.add('hide');
+                    // li[i].style.display = "";
+                } else {
+                    // li[i].style.display = "none";
+                    li[i].classList.remove('hide');
                 }
-                $('h1').click(function (event) {
-                    $('#input').val('');
-                    elem.classList.remove('hide');
-                    $('.in').hide();
-                });
-            });
 
-
-
-
-
-
-
-
-
-
-
-            list.forEach(elem => {
-
-                if (elem.innerText.search(value) == -1) {
-                    elem.classList.add('hide')
-                }
-                $('h1').click(function (event) {
-                    $('#input').val('');
-                    elem.classList.remove('hide');
-                    $('.in').hide();
-                });
-            });
-        } else {
-            list.forEach(elem => {
-                elem.classList.remove('hide');
-            });
-
-            listIF.forEach(elem => {
-                elem.classList.remove('hide');
-            });
+            }
         }
-        if ($('.hide').length == Object.keys(list).length) {
-            $('.in').show();
 
-        } else {
-            $('.in').hide();
 
-        }
     }
+    document.addEventListener('keyup', search);
+
+
+    // let input = document.querySelector('#input');
+    // input.oninput = function () {
+    //     let valu = this.value.trim();
+    //     let value = valu.toUpperCase();
+    //     let list = document.querySelectorAll('.mainPage li');
+    //     let listIF = document.querySelectorAll('.istorichniFotoPage li');
+
+
+    //     if (value != '') {
+
+    //         listIF.forEach(elem => {
+
+    //             if (elem.innerText.search(value) == -1) {
+    //                 elem.classList.add('hide')
+    //             }
+    //             $('h1').click(function (event) {
+    //                 $('#input').val('');
+    //                 elem.classList.remove('hide');
+    //                 $('.in').hide();
+    //             });
+    //         });
+
+
+
+
+
+
+
+
+
+
+
+    //         list.forEach(elem => {
+    //             console.log(list);
+
+    //             if (elem.innerText.search(value) == -1) {
+    //                 elem.classList.add('hide')
+    //             }
+    //             $('h1').click(function (event) {
+    //                 $('#input').val('');
+    //                 elem.classList.remove('hide');
+    //                 $('.in').hide();
+    //             });
+    //         });
+    //     } else {
+    //         list.forEach(elem => {
+    //             elem.classList.remove('hide');
+    //         });
+
+    //         listIF.forEach(elem => {
+    //             elem.classList.remove('hide');
+    //         });
+    //     }
+    //     if ($('.hide').length == Object.keys(list).length) {
+    //         $('.in').show();
+
+    //     } else {
+    //         $('.in').hide();
+
+    //     }
+    // }
 
     //------------------------------------------------------------------------------------------------------------------------------
 
